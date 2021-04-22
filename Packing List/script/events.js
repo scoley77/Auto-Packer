bcb7fea8256b9874652f15d4cae45fb12f5bf1b9
Sara Coley <79733957+scoley77@users.noreply.github.com>
2021-04-22 12:08:41 -0600

    Add files via upload

--- Packing List/script/events.js
+++ Packing List/script/events.js
@@ -1,6 +1,7 @@
 
 
 var buttonOne = function() {
+  var days = document.getElementById("days").value;
   document.getElementById("daysbtn").style.backgroundColor = "pink";
   var visOne = document.getElementById("weather-wrapper");
   visOne.style.visibility = "visible";
